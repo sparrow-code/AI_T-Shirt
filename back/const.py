73380aab54ae8e57d51045182ef21c80e83da951
sparrow-code <>
2024-11-30 22:13:12 +0530

    Structure Models

--- back/const.py
+++ back/const.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 
-ROOT_DIR = Path(__file__).parent.parent.resolve()
-OUTPUTS_DIR = "./output"
-LOG_DIR = "./logs"
+ROOT_DIR =  Path(__file__).resolve().parent
+OUTPUTS_DIR = ROOT_DIR / "output"
+LOG_DIR = ROOT_DIR / "logs"
 LOG_FILE = LOG_DIR / "server.log"
