7c3f962ad6b5565a3ed85c6f261abcc2ee355f21
sparrow-code <>
2024-12-04 03:51:41 +0530

    Payment Service

--- back/controller/payment.py
+++ back/controller/payment.py
@@ -5,11 +5,12 @@ from stripe import StripeClient
 class PaymentService:
     stripe.api_key = STRIPE_SECRET
 
-    def __init__(self):
+    def __init__(self, logger):
         self.stripe_client = StripeClient(api_key=STRIPE_API)
+        self.logger = logger
         pass
 
-    def create_pay_session(self, amount, currency, name):
+    async def create_pay_session(self, amount, currency, name):
         try:
             session = stripe.checkout.Session.create(
                billing_address_collection="auto",
@@ -24,21 +25,28 @@ class PaymentService:
                     'quantity': 1,
                 }],
                 mode="payment",
-                success_url= BASE_URL + "/api/payment/success",
-                cancel_url= BASE_URL + "/api/payment/cancel",
+                success_url= "{BASE_URL}/api/payment/success?session_id={CHECKOUT_SESSION_ID}",
+                cancel_url= "{BASE_URL}/api/payment/cancel?session_id={CHECKOUT_SESSION_ID}",
             )
             return session  # Return session object for further use
         except stripe.error.StripeError as e:
             print(f"Error occurred: {e}")
             return None
 
-    def confirm_payment_intent(self, payment_intent_id):
-        return self.stripe_client.payment_intents.confirm(payment_intent_id)
-    
-    def cancel_payment_intent(self, payment_intent_id):
-        return self.stripe_client.payment_intents.cancel(payment_intent_id)
-    
-    def retrieve_payment_intent(self, payment_intent_id):
-        return self.stripe_client.payment_intents.retrieve(payment_intent_id)
-    
-    
+    async def payment_success(self, session_id):
+        try:
+            session = stripe.checkout.Session.retrieve(session_id)
+            self.logger.info(session)
+            return session
+        except stripe.error.StripeError as e:
+            print(f"Error occurred: {e}")
+            return None
+
+    async def payment_cancel(self, session_id):
+        try:
+            session = stripe.checkout.Session.retrieve(session_id)
+            self.logger.info(session)
+            return session
+        except stripe.error.StripeError as e:
+            print(f"Error occurred: {e}")
+            return None
