2873d157ca5b0fbcb4575dd86f0e30184e1b17ab
sparrow-code <>
2024-12-20 00:16:04 +0530

    improve dev

--- back/main.py
+++ back/main.py
@@ -10,7 +10,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 
 origins = [
-    "http://localhost.tiangolo.com",
+    "https://poodle-feasible-sadly.ngrok-free.app",
     "https://aitshirts.in",
     "http://localhost:8000",
     "http://localhost:3000",
